68bc7fbd9fb55a5b11f29653989d00169ee8b2a4
Robbie <tranxuanthanhpbe@gmail.com>
2025-03-12 11:23:42 +0700

    test

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ function checkLogin(event){
     event.preventDefault();
 
     let name = document.getElementById("name").value;
-    let dob = document.getElementById("dateofbirth").value;
+    let dob = document.getElementById("dob").value;
     let id = document.getElementById("id-number").value;
     let address = document.getElementById("address").value;
 
     if (name === "" || dob === "" || id === "" || address === ""){
-        alert(address);
+        alert("Please fill in all information");
     }
     else{
         if(String(id).length !== 12){
             alert("ID number must be 12 numbers long");
         }
+        else if(dob > Date.now){
+            alert("Date of birth is invalid");
+        }else
         window.location.href='tracno.html';
     }
+}
+
+function survey(){
+    
 }
