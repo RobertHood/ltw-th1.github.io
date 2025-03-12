2297a5dd6bc84fb6accadd1493966b2b7bd86bfe
Robbie <tranxuanthanhpbe@gmail.com>
2025-03-12 13:05:59 +0700

    aaaa

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+global_name = "";
+global_dob = "";
+global_id = "";
+global_add = "";
+
 function checkLogin(event){
     event.preventDefault();
-
     let name = document.getElementById("name").value;
     let dob = document.getElementById("dob").value;
     let id = document.getElementById("id-number").value;
     let address = document.getElementById("address").value;
 
+    global_name = name;
+    global_dob = dob;
+    global_id = id;
+    global_add = address;    
     if (name === "" || dob === "" || id === "" || address === ""){
         alert("Please fill in all information");
     }
@@ -20,6 +28,21 @@ function checkLogin(event){
     }
 }
 
-function survey(){
+function survey(event){
+    event.preventDefault();
+
+    const urlParams = new URLSearchParams(window.location.search);
     
+    
+    let survey_info_name = document.getElementById("taker-name");
+    let survey_info_dob = document.getElementById("taker-dob");
+    let survey_info_id = document.getElementById("taker-id");
+    let survey_info_add = document.getElementById("taker-add");
+
+    survey_info_name.textContent = "Name: " + global_name;
+    survey_info_dob.textContent = "DOB: " + global_dob;
+    survey_info_id.textContent = "ID: " + global_id;
+    survey_info_add.textContent = "Add: " + global_add; 
+
+    return;
 }
